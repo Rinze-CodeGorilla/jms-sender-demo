3b4f60e0d98507a27c4fe536fcd5539ae9bf4cc3
Rinze-CodeGorilla <132885446+Rinze-CodeGorilla@users.noreply.github.com>
2024-08-13 16:56:14 +0200

    Initial commit

--- src/main/java/nl/codegorilla/jmssenderdemo/JmsSenderDemoApplication.java
+++ src/main/java/nl/codegorilla/jmssenderdemo/JmsSenderDemoApplication.java
@@ -3,6 +3,8 @@ package nl.codegorilla.jmssenderdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class JmsSenderDemoApplication {
 
@@ -10,4 +12,14 @@ public class JmsSenderDemoApplication {
         SpringApplication.run(JmsSenderDemoApplication.class, args);
     }
 
+    JmsSenderDemoApplication(SenderFactory senderFactory) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What is your name?");
+        Sender sender = senderFactory.createSender(s.nextLine().trim());
+        while(true) {
+            System.out.println("Please enter the message to send: ");
+            String message = s.nextLine().trim();
+            sender.send(message);
+        }
+    }
 }
